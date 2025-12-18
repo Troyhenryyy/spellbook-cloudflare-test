76f83d889306608a7c6ab835e8c8868e32ad7834
Troyoyo <134513646+Troyhenryyy@users.noreply.github.com>
2025-12-18 19:11:15 +0800

    adding cloudflare assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,19 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   // Preact goes here (Astro Integration)
   integrations: [preact()],
-  
+
   // Tailwind v4 goes here (Vite Plugin)
   vite: {
     plugins: [tailwindcss()],
   },
 
   // This silences the "Server Routes" warning for now
-  output: 'server', 
+  output: 'server',
+
+  adapter: cloudflare(),
 });
